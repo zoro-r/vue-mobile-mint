d0f16888c88abaece506822da21eee61761df9f8
江道丰 <jiangdaofeng@n22.com.cn>
2017-12-01 15:40:12 +0800

    update

--- src/pages/login/StartupScreen.vue
+++ src/pages/login/StartupScreen.vue
@@ -3,12 +3,13 @@
 		  <!-- swiper -->
       <swiper :options="swiperOption" ref="mySwiper">
         <swiper-slide class="swiper_base first">
-					<img class="one ani" src="./../../assets/img/screen/1.png" swiper-animate-effect="fadeInDownBig" swiper-animate-duration="1s" swiper-animate-delay="0.3s">
-					<img class="two ani" src="./../../assets/img/screen/2.png"  swiper-animate-effect="fadeInUpBig" swiper-animate-duration="1s" swiper-animate-delay="0.3s">
+					<img class="one ani" src="./../../assets/img/screen/1.png" swiper-animate-effect="bounceInDown" swiper-animate-duration="1s" swiper-animate-delay="0s">
+					<img class="two ani" src="./../../assets/img/screen/2.png"  swiper-animate-effect="fadeInUpBig" swiper-animate-duration=".5s" swiper-animate-delay="0s">
 					<!-- <img class="three" src="./../../assets/img/screen/3.png" alt=""> -->
 				</swiper-slide>
         <swiper-slide class="swiper_base">Slide 2</swiper-slide>
         <swiper-slide class="swiper_base">Slide 3</swiper-slide>
+				<div class="swiper-pagination" slot="pagination"></div>
       </swiper>
 	</div>
 </template>
@@ -22,16 +23,20 @@ export default {
       swiperOption: {
         direction: "horizontal",
         effect: "fade",
-				height: 500,
-				on:{
-					init(){
-						swiperAnimateCache(this); //隐藏动画元素 
-						swiperAnimate(this); //初始化完成开始动画
-					}, 
-					slideChangeTransitionEnd(){ 
-						swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
-					} 
-				}
+        height: 500,
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true
+        },
+        on: {
+          init() {
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+          },
+          slideChangeTransitionEnd() {
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+          }
+        }
       }
     };
   },
@@ -52,21 +57,27 @@ export default {
 @import "src/assets/css/vars";
 .swiper_base {
   height: 100vh;
-	font-size: 1.5rem;
-	&.first{
-		box-sizing: border-box;
-		// display: flex;
-		text-align: center;
-		// justify-content: center;
-		background: -webkit-gradient(linear, left top,left bottom, from(rgb(65, 112, 227)), to(rgb(255,255,255)));
-		.one{
-			height: 55%;
-			margin: 5% 10px;
-		}
-		.two{
-			height: 40%;
-			width: 100%;
-		}
-	}
+  font-size: 1.5rem;
+  &.first {
+    box-sizing: border-box;
+    // display: flex;
+    text-align: center;
+    // justify-content: center;
+    background: -webkit-gradient(
+      linear,
+      left top,
+      left bottom,
+      from(rgb(65, 112, 227)),
+      to(rgb(255, 255, 255))
+    );
+    .one {
+      height: 55%;
+      margin: 5% 10px;
+    }
+    .two {
+      height: 40%;
+      width: 100%;
+    }
+  }
 }
 </style>
